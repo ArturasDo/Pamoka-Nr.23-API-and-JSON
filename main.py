7874096356b95b6f4c2cbcc55c35efa114b3bf9f
ArturasDo <48279198+ArturasDo@users.noreply.github.com>
2019-05-21 18:24:16 +0300

    API_KEY

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 
+
 app = Flask(__name__)
 
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def index():
     query = "London,UK"
     unit = "metric"  # use "imperial" for Fahrenheit
-    api_key = "<>ba96c72f23ee449cfd62c8cefc1f03fb"
+    api_key = "API_KEY"
 
     url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}".format(query, unit, api_key)
     data = requests.get(url=url)  # GET request to the OpenWeatherMap API
